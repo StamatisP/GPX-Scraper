f6d27d8fd2acea9b447eef754a8a9ed0e39e5b57
StamatisP <StamatisP@users.noreply.github.com>
2019-12-26 22:55:51 -0500

    bit more work, i forgot to get the users gender too

--- GPX-Scraper/Functions.cs
+++ GPX-Scraper/Functions.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.Generic;
+using SeleniumExtras.WaitHelpers;
 
 namespace GPX_Scraper
 {
@@ -23,6 +24,7 @@ namespace GPX_Scraper
         public string Journal;
         public string Status;
         public string Affinity;
+        public string Gender;
         public PokeData[] Party;
     }
     public class Functions
@@ -40,27 +42,44 @@ namespace GPX_Scraper
         public UserData GetUserData(string UserName)
         {
             UserData data = new UserData();
-            driver.Navigate().GoToUrl("https://gpx.plus/user/" + UserName);
-            WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(5));
-            wait.Until(driver => driver.FindElement(By.Id("userInfo"))); // confirm connected
 
-            //Name
+            driver.Navigate().GoToUrl("https://gpx.plus/user/" + UserName);
+
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(5));
+                wait.Until(ExpectedConditions.ElementExists(By.Id("userInfo"))); // confirm connected
+            } 
+            catch (WebDriverTimeoutException e)
+            {
+                Console.WriteLine("\n\nProfile does not exist, press enter to exit.\n\n");
+                Console.ReadLine();
+                driver.Quit();
+                Environment.Exit(1);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            #region Name
             var namew = Stopwatch.StartNew();
             IWebElement element = driver.FindElement(By.Id("userInfo"));
             //Console.WriteLine(element.Text);
             data.Name = element.FindElement(By.TagName("div")).Text.Trim();
             namew.Stop();
             Console.WriteLine("User Name Elapsed={0}", namew.Elapsed);
+            Console.WriteLine("The name of this user is " + data.Name);
+            #endregion
 
-            //Journal
+            #region Journal
             var journalw = Stopwatch.StartNew();
             element = driver.FindElement(By.Id("userJournal"));
             element = element.FindElement(By.TagName("article"));
             data.Journal = element.Text;
             journalw.Stop();
             Console.WriteLine("Journal Elapsed={0}", journalw.Elapsed);
+            Console.WriteLine("Their journal currently reads: \n" + data.Journal + "\n");
+            #endregion
 
-            //Status
+            #region Status
             var statusw = Stopwatch.StartNew();
             element = driver.FindElement(By.Id("userStatus"));
             element = element.FindElement(By.TagName("div"));
@@ -68,6 +87,8 @@ namespace GPX_Scraper
             data.Status = element.Text;
             statusw.Stop();
             Console.WriteLine("User Status Elapsed={0}", statusw.Elapsed);
+            Console.WriteLine("Their status is currently \"" + data.Status + "\"");
+            #endregion
 
             #region Affinity
             element = driver.FindElement(By.Id("userInfo"));
@@ -76,6 +97,7 @@ namespace GPX_Scraper
                 element = element.FindElement(By.CssSelector("[class^=SpriteAffinity]"));
                 string affinity = element.GetAttribute("class").Remove(0, 14);
                 data.Affinity = affinity;
+                Console.WriteLine("Their affinity is " + affinity + ".");
             }
             catch (NoSuchElementException e)
             {
@@ -83,7 +105,7 @@ namespace GPX_Scraper
             }
             #endregion
 
-            //Party
+            #region Party
             var partyw = Stopwatch.StartNew();
             element = driver.FindElement(By.Id("UserParty"));
             var party = driver.FindElements(By.ClassName("PartyPoke"));
@@ -138,6 +160,8 @@ namespace GPX_Scraper
             }
             partyw.Stop();
             Console.WriteLine("User Party Elapsed={0}", partyw.Elapsed);
+            #endregion
+            Console.ForegroundColor = ConsoleColor.Gray;
 
             return data;
         }
@@ -147,20 +171,41 @@ namespace GPX_Scraper
             PokeData pokedata = new PokeData();
 
             driver.Navigate().GoToUrl("https://gpx.plus/info/" + dataName);
-            WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(5));
-            wait.Until(driver => driver.FindElement(By.Id("infoTable"))); // confirm connected
 
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, System.TimeSpan.FromSeconds(5));
+                wait.Until(ExpectedConditions.ElementExists(By.Id("infoTable"))); // confirm connected
+            }
+            catch (WebDriverTimeoutException e)
+            {
+                Console.WriteLine("\n\nPokemon does not exist, press enter to exit.\n\n");
+                Console.ReadLine();
+                driver.Quit();
+                Environment.Exit(1);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
             #region Name and Nickname
             IWebElement element = driver.FindElement(By.Id("infoPokemon"));
             var emelements = element.FindElements(By.TagName("em"));
             pokedata.Nickname = emelements[0].Text;
             pokedata.Name = emelements[1].Text.Trim().Remove(0, 1);
+            if (pokedata.Nickname == "No nickname")
+            {
+                Console.WriteLine("The name of this Pokemon is " + pokedata.Name);
+            }
+            else
+            {
+                Console.WriteLine("The nickname of this Pokemon is " + pokedata.Nickname);
+            }
             #endregion
 
             #region Nature
             element = driver.FindElement(By.Id("infoPokemon"));
             element = element.FindElement(By.ClassName("tip"));
             pokedata.Nature = element.Text;
+            Console.WriteLine("This Pokemon has a " + pokedata.Nature + " nature.");
             #endregion
 
             #region Sprite
@@ -176,24 +221,27 @@ namespace GPX_Scraper
             {
                 element = element.FindElement(By.CssSelector("[class^=SpriteGender]"));
                 string gender = element.GetAttribute("class").Remove(0, 12);
-                Console.WriteLine(gender);
                 pokedata.Gender = gender;
             }
             catch (NoSuchElementException e)
             {
-                Console.WriteLine("No spritegender found, must be genderless");
                 pokedata.Gender = "Genderless";
             }
+            Console.WriteLine("This pokemon is " + pokedata.Gender);
             #endregion
 
             #region Owner
             element = driver.FindElement(By.Id("infoPokemon"));
             element = element.FindElement(By.TagName("a"));
+            pokedata.Owner = element.Text;
+            Console.WriteLine("The owner of this Pokemon is " + pokedata.Owner);
             #endregion
 
             #region Id
             pokedata.Id = dataName;
+
             #endregion
+            Console.ForegroundColor = ConsoleColor.Gray;
 
             return pokedata;
         }
--- TestScraper/Program.cs
+++ TestScraper/Program.cs
@@ -8,19 +8,22 @@ namespace TestScraper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a username.");
+            string username = Console.ReadLine();
+            Console.WriteLine("Please enter a pokemon ID (https://gpx.plus/info/-->fiwZp<--)");
+            string pokeid = Console.ReadLine();
             var scraper = new GPX_Scraper.Functions();
 
             var userd = Stopwatch.StartNew();
-            scraper.GetUserData("StamosYeah");
+            scraper.GetUserData(username);
             userd.Stop();
             Console.WriteLine("UserData Elapsed={0}", userd.Elapsed);
 
             var poked = Stopwatch.StartNew();
-            scraper.GetPokeData("fiwZp");
+            scraper.GetPokeData(pokeid);
             poked.Stop();
             Console.WriteLine("PokeData Elapsed={0}", poked.Elapsed);
-            Console.WriteLine("Done!");
+            Console.WriteLine("Done! Please type \"exit\" to safely close the program.");
             var text = Console.ReadLine();
             if (text == "exit")
             {
